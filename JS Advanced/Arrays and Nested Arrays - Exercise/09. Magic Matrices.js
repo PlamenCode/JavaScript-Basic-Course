33ae6e8c6102d8ddc02036a9274bd59421b113f2
PlamenCode <plamenggg233@gmail.com>
2023-01-15 23:39:56 +0200

    Update 09. Magic Matrices.js

--- JS Advanced/Arrays and Nested Arrays - Exercise/09. Magic Matrices.js
+++ JS Advanced/Arrays and Nested Arrays - Exercise/09. Magic Matrices.js
@@ -1,7 +1,22 @@
 function magicMatrix(matrix){
+    let isMagical = true;
 
-    
+    for(let i = 0; i < matrix.length - 1; i++){
+        let currenRowSum = matrix[i].reduce((a,b) => a+b, 0);
+        let nextRowSum = matrix[i+1].reduce((a,b) => a+b, 0);
+        let currentColSum = 0;
+        let nextColSum = 0;
 
+        for(let j = 0; j < matrix.length; j++){
+            currentColSum += matrix[i][j];
+            nextColSum += matrix[i + 1][j]
+        }
+
+        if(currenRowSum !== nextRowSum || currentColSum !== nextColSum){
+            isMagical = false;
+        }
+    }
+    return isMagical;
 }
 
 magicMatrix([
