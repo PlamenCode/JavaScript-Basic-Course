eafbb41c772f143b7016f9db6b01cb7377392bca
PlamenCode <114262188+PlamenCode@users.noreply.github.com>
2023-03-01 22:27:53 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function checkFlowersAvailable(flower, gardenArr) {
-  if (gardenArr.indexOf(flower) >= 0) {
-      console.log( `The ${flower} are available!`)
-  } else {
-      console.log( `The ${flower} are sold! You need to purchase more!`);
-  }
-}
-checkFlowersAvailable('Rose', ["Rose", "Lily", "Orchid"])
